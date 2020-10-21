770325e67e7804b683843226eab1b6fc0bf35133
ohnewdev <ohnewdev@gmail.com>
2020-10-21 20:43:03 +0900

    Add Stock Data.

--- SamplingAndPAA/Form1.Designer.cs
+++ SamplingAndPAA/Form1.Designer.cs
@@ -28,48 +28,75 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.axKHOpenAPI1 = new AxKHOpenAPILib.AxKHOpenAPI();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axKHOpenAPI1)).BeginInit();
             this.SuspendLayout();
             // 
             // chart1
             // 
-            chartArea3.Name = "ChartArea1";
-            chartArea4.Name = "ChartArea2";
-            this.chart1.ChartAreas.Add(chartArea3);
-            this.chart1.ChartAreas.Add(chartArea4);
-            legend2.Name = "Legend1";
-            this.chart1.Legends.Add(legend2);
+            chartArea1.AxisX.IsReversed = true;
+            chartArea1.AxisY.IsStartedFromZero = false;
+            chartArea1.Name = "ChartArea1";
+            chartArea2.AxisX.IsReversed = true;
+            chartArea2.AxisY.IsStartedFromZero = false;
+            chartArea2.Name = "ChartArea2";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
             this.chart1.Location = new System.Drawing.Point(12, 36);
             this.chart1.Name = "chart1";
-            series3.ChartArea = "ChartArea1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            series2.ChartArea = "ChartArea2";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Legend = "Legend1";
+            series2.Name = "Series2";
+            series3.ChartArea = "ChartArea2";
             series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
             series3.Legend = "Legend1";
-            series3.Name = "Series1";
-            series4.ChartArea = "ChartArea2";
-            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
-            series4.Legend = "Legend1";
-            series4.Name = "Series2";
+            series3.Name = "Series3";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
             this.chart1.Series.Add(series3);
-            this.chart1.Series.Add(series4);
-            this.chart1.Size = new System.Drawing.Size(715, 347);
+            this.chart1.Size = new System.Drawing.Size(1104, 630);
             this.chart1.TabIndex = 0;
             this.chart1.Text = "chart1";
+            // 
+            // axKHOpenAPI1
+            // 
+            this.axKHOpenAPI1.Enabled = true;
+            this.axKHOpenAPI1.Location = new System.Drawing.Point(305, -4);
+            this.axKHOpenAPI1.Name = "axKHOpenAPI1";
+            this.axKHOpenAPI1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axKHOpenAPI1.OcxState")));
+            this.axKHOpenAPI1.Size = new System.Drawing.Size(100, 50);
+            this.axKHOpenAPI1.TabIndex = 1;
+            this.axKHOpenAPI1.Visible = false;
+            this.axKHOpenAPI1.OnReceiveTrData += new AxKHOpenAPILib._DKHOpenAPIEvents_OnReceiveTrDataEventHandler(this.axKHOpenAPI1_OnReceiveTrData);
+            this.axKHOpenAPI1.OnEventConnect += new AxKHOpenAPILib._DKHOpenAPIEvents_OnEventConnectEventHandler(this.axKHOpenAPI1_OnEventConnect);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(871, 499);
+            this.ClientSize = new System.Drawing.Size(1149, 678);
+            this.Controls.Add(this.axKHOpenAPI1);
             this.Controls.Add(this.chart1);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axKHOpenAPI1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -77,6 +104,7 @@
         #endregion
 
         private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private AxKHOpenAPILib.AxKHOpenAPI axKHOpenAPI1;
     }
 }
 
--- SamplingAndPAA/Form1.cs
+++ SamplingAndPAA/Form1.cs
@@ -15,29 +15,43 @@ namespace SamplingAndPAA
     public partial class Form1 : Form
     {
         double[] rawData = { 15, 29, 21, 45, 23, 63, 54, 12, 54, 61, 44, 56, 23, 56, 21, 44, 32, 46, 23, 38 };
+
+        private Series s1;
+        private Series s2;
+        private Series s3;
+
+
         public Form1()
         {
             InitializeComponent();
-            Series s1 = chart1.Series[0];
-            Series s2 = chart1.Series[1];
 
-            for (int i = 0; i < rawData.Length; i++)
-            {
-                s1.Points.AddXY(i + 1, rawData[i]);
-            }
-            double[] sampleData = sampling(rawData, 10);
-            //for (int i = 0; i < sampleData.Length; i++)
+
+            s1 = chart1.Series[0];
+            s2 = chart1.Series[1];
+            s3 = chart1.Series[2];
+
+
+            axKHOpenAPI1.CommConnect();
+
+
+
+            //for (int i = 0; i < rawData.Length; i++)
+            //{
+            //    s1.Points.AddXY(i + 1, rawData[i]);
+            //}
+            //double[] sampleData = sampling(rawData, 10);
+            ////for (int i = 0; i < sampleData.Length; i++)
+            ////{
+
+            ////    s2.Points.AddXY(i + 1, sampleData[i]);
+            ////}
+
+            //double[] paaData = PAA(rawData, 4);
+            //for (int i = 0; i < paaData.Length; i++)
             //{
 
-            //    s2.Points.AddXY(i + 1, sampleData[i]);
+            //    s2.Points.AddXY(i + 1, paaData[i]);
             //}
-
-            double[] paaData = PAA(rawData, 4);
-            for (int i = 0; i < paaData.Length; i++)
-            {
-
-                s2.Points.AddXY(i + 1, paaData[i]);
-            }
         }
         public static double[] sampling(double[] rawData, int n)
         {
@@ -79,6 +93,49 @@ namespace SamplingAndPAA
             return paaData;
         }
 
+        private void axKHOpenAPI1_OnEventConnect(object sender, AxKHOpenAPILib._DKHOpenAPIEvents_OnEventConnectEvent e)
+        {
+            if (e.nErrCode == 0)
+            {
+                axKHOpenAPI1.SetInputValue("종목코드", "005930");
+                axKHOpenAPI1.SetInputValue("기준일자", DateTime.Now.ToString("yyyyMMdd"));
+                axKHOpenAPI1.SetInputValue("수정주가구분", "0");
+                axKHOpenAPI1.CommRqData("일봉차트", "opt10081", 0, "1001");
+
+            }
+        }
+
+        private void axKHOpenAPI1_OnReceiveTrData(object sender, AxKHOpenAPILib._DKHOpenAPIEvents_OnReceiveTrDataEvent e)
+        {
+            int count = axKHOpenAPI1.GetRepeatCnt(e.sTrCode, e.sRQName);
+            double[] rawData = new double[count];
+            for (int i = 0; i < count; i++)
+            {
+                rawData[i] = double.Parse(axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, i, "현재가"));
+
+            }
+            for (int i = 0; i < rawData.Length; i++)
+            {
+
+                s1.Points.AddXY(i + 1, rawData[i]);
+            }
+
+            double[] sampleData = sampling(rawData, 15);
+            for (int i = 0; i < sampleData.Length; i++)
+            {
+
+                s2.Points.AddXY(i + 1, sampleData[i]);
+            }
+
+            double[] paaData = PAA(rawData, 15);
+            for (int i = 0; i < paaData.Length; i++)
+            {
+
+                s3.Points.AddXY(i + 1, paaData[i]);
+            }
+
+
+        }
     }
 
 }
