e27ec1965035c592be96530d55cd59db898c2b11
ohnewdev <ohnewdev@gmail.com>
2020-10-21 19:53:16 +0900

    Add project files.

---
+++ SamplingAndPAA/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace SamplingAndPAA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea3.Name = "ChartArea1";
+            chartArea4.Name = "ChartArea2";
+            this.chart1.ChartAreas.Add(chartArea3);
+            this.chart1.ChartAreas.Add(chartArea4);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(12, 36);
+            this.chart1.Name = "chart1";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            series4.ChartArea = "ChartArea2";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series4.Legend = "Legend1";
+            series4.Name = "Series2";
+            this.chart1.Series.Add(series3);
+            this.chart1.Series.Add(series4);
+            this.chart1.Size = new System.Drawing.Size(715, 347);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(871, 499);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ SamplingAndPAA/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace SamplingAndPAA
+{
+    public partial class Form1 : Form
+    {
+        double[] rawData = { 15, 29, 21, 45, 23, 63, 54, 12, 54, 61, 44, 56, 23, 56, 21, 44, 32, 46, 23, 38 };
+        public Form1()
+        {
+            InitializeComponent();
+            Series s1 = chart1.Series[0];
+            Series s2 = chart1.Series[1];
+
+            for (int i = 0; i < rawData.Length; i++)
+            {
+                s1.Points.AddXY(i + 1, rawData[i]);
+            }
+            double[] sampleData = sampling(rawData, 10);
+            //for (int i = 0; i < sampleData.Length; i++)
+            //{
+
+            //    s2.Points.AddXY(i + 1, sampleData[i]);
+            //}
+
+            double[] paaData = PAA(rawData, 4);
+            for (int i = 0; i < paaData.Length; i++)
+            {
+
+                s2.Points.AddXY(i + 1, paaData[i]);
+            }
+        }
+        public static double[] sampling(double[] rawData, int n)
+        {
+            double[] samplingData = new double[n];
+            int interval = rawData.Length / n;
+
+            for (int i = 0; i < n; i++)
+            {
+                samplingData[i] = rawData[i * interval];
+
+            }
+
+            return samplingData;
+        }
+
+        public static double[] PAA(double[] rawData, int n)
+        {
+            double[] paaData = new double[n];
+            int interval = rawData.Length / n;
+            for (int i = 0; i < n; i++)
+            {
+                double sum = 0;
+                int count = 0;
+                for (int j = interval * i; j < interval * (i + 1); j++)
+                {
+                    sum += rawData[j];
+                    count++;
+
+                }
+
+                if (count != 0)
+                {
+                    double avg = sum / count;
+                    paaData[i] = avg;
+
+                }
+            }
+
+            return paaData;
+        }
+
+    }
+
+}
